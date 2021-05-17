e42cc76f50a2a4c85a53de0991ff29d412d89fec
Morten Bertz <morten@telethon.jp>
2021-05-17 16:36:27 +0900

    Initial Commit

---
+++ GaussMeter/GaussMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaussMeterApp.swift
+//  GaussMeter
+//
+//  Created by Morten Bertz on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct GaussMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
